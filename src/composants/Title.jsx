19d6b473de78c17a156a03359bc46f6f7cac5a72
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2024-05-29 16:45:42 +0200

    Mise en place des différents éléménts nécssaires à l'édition des données de l'utilisateur.

--- src/composants/Title.jsx
+++ src/composants/Title.jsx
@@ -1,10 +1,40 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
 const Title = () => {
+  const [isEditingUser, setIsEditingUser] = useState(false)
+  const userData = useSelector((state) => state.auth.user)
+
+  // Ajouter une vérification pour s'assurer que userData est défini
+  if (!userData) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='userTitle'>
-      <h1>
-        Welcome back <br /> Tony Jarvis!
-      </h1>
-      <button className='editButton'>Edit Name</button>
+      {!isEditingUser && (
+        <h1>
+          Welcome back <br /> {userData.firstName} {userData.lastName}
+        </h1>
+      )}
+      {isEditingUser && (
+        <form className='userForm'>
+          <input type='text' name='userName' value='userData.userName' />
+          <input type='text' name='firstName' value='userData.firstName' />
+          <input type='text' name='lastName' value='userData.lastName' />
+          <input type='submit' value='Enregistrer' className='userFormSubmit' />
+          <button
+            type='button'
+            onClick={() => setIsEditingUser(false)}
+            className='userFormButton'
+          >
+            Annuler
+          </button>
+        </form>
+      )}
+      <button className='editButton' onClick={() => setIsEditingUser(true)}>
+        Edit Name
+      </button>
       <h2 className='sr-only'>Accounts</h2>
     </div>
   )
