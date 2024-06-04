52f158cc6ed5d1bd0f2fda50a7d38cdac39f6ec7
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2024-06-04 16:26:42 +0200

    Modificarion de la bannière pour optimiser les imlages.

--- src/composants/Hero.jsx
+++ src/composants/Hero.jsx
@@ -1,6 +1,16 @@
 const Hero = () => {
   return (
     <section className='hero'>
+      <img
+        src='../../src/assets/img/bank-tree.jpeg'
+        srcSet="
+        '../../src/assets/img/bank-tree-1100.webp 1100w'
+        '../../src/assets/img/bank-tree-700.webp 700w'
+        '../../src/assets/img/bank-tree-500.webp 500w'
+        "
+        alt="Photo d'une plante en pot"
+        className='heroBanner'
+      />
       <div className='heroTxt'>
         <h2 className='sr-only'>Promoted Content</h2>
         <p className='subtitle'>No fees.</p>
