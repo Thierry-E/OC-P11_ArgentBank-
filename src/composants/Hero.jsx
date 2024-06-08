dd047fa35ae2a3f79ccc6f0b93fb3847c912d8d5
Thierry-E <76159642+Thierry-E@users.noreply.github.com>
2024-06-08 16:32:37 +0200

    Correction d'une erreur dans l'attribut "srcset" de l'élément "img" situé au sein du composant "Hero".

--- src/composants/Hero.jsx
+++ src/composants/Hero.jsx
@@ -3,11 +3,10 @@ const Hero = () => {
     <section className='hero'>
       <img
         src='../../src/assets/img/bank-tree.webp'
-        srcSet="
-        '../../src/assets/img/bank-tree-1100.webp 1100w'
-        '../../src/assets/img/bank-tree-700.webp 700w'
-        '../../src/assets/img/bank-tree-500.webp 500w'
-        "
+        srcSet='
+        ../../src/assets/img/bank-tree-1100.webp 1100w,
+        ../../src/assets/img/bank-tree-700.webp 700w,
+        ../../src/assets/img/bank-tree-500.webp 500w'
         alt="Photo d'une plante en pot"
         className='heroBanner'
       />
